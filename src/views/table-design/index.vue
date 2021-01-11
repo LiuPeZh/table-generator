6d8a5e6816c37a63903e0d76a3704f081ee88006
刘佩智 <liupeizhi@gridsum.com>
2021-01-11 18:07:01 +0800

    init

---
+++ src/views/table-design/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="table-design">
+    <div class="table-design-left"></div>
+    <div class="table-design-main"></div>
+    <div class="table-design-right"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const testNum = ref<number>(0)
+
+    return {
+      testNum
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.table-design {
+  * {
+    outline: 1px solid red;
+  }
+  display: flex;
+  height: 100vh;
+  .table-design-left,
+  .table-design-main,
+  .table-design-right {
+    height: 100%;
+  }
+  .table-design-left {
+    flex: 0 0 300px;
+  }
+  .table-design-main {
+    flex: 1 0 auto;
+  }
+  .table-design-right {
+    flex: 0 0 200px;
+  }
+}
+</style>
